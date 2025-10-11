899995287c01846783b50c0b47a6288c27f99c75
Nguyen Hoang Minh Quan <quannhmts01396@fpt.edu.vn>
2025-10-11 12:25:01 +0700

    update

---
+++ src/main/java/poly/edu/dao/DiaChiDAO.java
@@ -0,0 +1,6 @@
+package poly.edu.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import poly.edu.entity.*;
+
+public interface DiaChiDAO extends JpaRepository<DiaChi, Integer> {}
