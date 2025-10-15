0da5e6a50f0b88bfc135fd2f371e8a8432cedbd0
Nguyen Hoang Minh Quan <quannhmts01396@fpt.edu.vn>
2025-10-15 19:56:15 +0700

    Dang nhap cookie & Ql tai khoan

--- src/main/java/poly/edu/controller/EmployeeController.java
+++ src/main/java/poly/edu/controller/EmployeeController.java
@@ -1,23 +1,36 @@
 package poly.edu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import poly.edu.service.AuthService;
+import poly.edu.service.ParamService;
+
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
-
-    @GetMapping("/dashboard")
+	
+	@Autowired
+	private AuthService authService;
+	@Autowired
+	private ParamService paramService;
+	
+	@GetMapping("/dashboard")
     public String dashboard(Model model) {
+        if (!authService.isAdmin()) {
+            return "redirect:/employee/products";
+        }
+        
         model.addAttribute("title", "Dashboard - Thống kê");
         model.addAttribute("role", "employee");
         return "employee/NV_index";
     }
 
     @GetMapping("/products")
-    public String products(Model model) {
+    public String products(Model model) { 
         model.addAttribute("title", "Quản lý sản phẩm");
         model.addAttribute("role", "employee");
         return "employee/NV_QLsanpham";
